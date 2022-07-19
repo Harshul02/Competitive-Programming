ef7b44b12d5a63c851559dad6048d5f058598b28
Harshul Jain <harshulgangwal02@gmail.com>
2022-07-19 17:56:16 +0530

    Max Score

---
+++ STL/Maximum score from removing stone.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int maximumScore(int a, int b, int c) {
+     priority_queue<int> q;
+        q.push(a);
+        q.push(b);
+        q.push(c);
+        int z=0;
+        while(q.size()>1)
+        {
+            int x=q.top();
+            q.pop();
+            x--;
+            int y=q.top();
+            q.pop();
+            y--;
+            if(x!=0)
+                q.push(x);
+            if(y!=0)
+                q.push(y);
+            z++;
+        }
+        return z;
+}
